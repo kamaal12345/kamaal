95de66145218d7252d58905b2b34acd798839bf3
kamaal12345 <109724907+kamaal12345@users.noreply.github.com>
2023-04-04 12:29:32 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,39 +1,13 @@
-provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
-}
-
-resource "aws_instance" "kamaal" {
-  ami           = "ami-00c39f71452c08778"
-  instance_type = "t2.micro"
-   network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
-}
-
- AutoScalingConfig:
-    Type: AWS::AutoScaling::LaunchConfiguration
+Resources:
+  MyEC2Instance:
+    Type: 'AWS::EC2::Instance'
     Properties:
-      ...
-      BlockDeviceMappings:
-      - DeviceName: "/dev/sdk"
-+       Ebs:
-          ...
-+         Encrypted: true
-      - DeviceName: "/dev/sdf"
-        Ebs:
-            Encrypted: false
-            - DeviceName: "/dev/sdc"
- VirtualName: ephermal
- 
-
-
-
-
-
+      ImageId: ami-00c39f71452c08778
+      InstanceType: t2.micro
+      KeyName: my-key-pair
+      SecurityGroupIds:
+        - my-security-group
+      Tags:
+        - Key: Name
+          Value: example-instance
 
