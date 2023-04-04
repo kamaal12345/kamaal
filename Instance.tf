3b5742dd1f8839f21e596923e6cfb1572044e2e1
kamaal12345 <109724907+kamaal12345@users.noreply.github.com>
2023-04-04 13:10:02 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,17 +1,17 @@
 # Define the AWS provider
-provider "aws" {
+- provider: "aws" {
   region = "us-east-1"
   access_key = "AKIAVWY4KPJQKK5YWDXL"
   secret_key = "0XIQaE6pKkL9wj12tg7mkngX4yC6DpFBQdlcdtx3"
 }
 
 # Create a new EC2 instance
-resource "aws_instance" "example" {
-  ami           = "ami-00c39f71452c08778"
-  instance_type = "t2.micro"
-  key_name      = "my-key-pair"
-  security_groups = ["my-security-group"]
-  tags = {
+-  resource:  "aws_instance" "example" {
+    ami           = "ami-00c39f71452c08778"
+    instance_type = "t2.micro"
+    key_name      = "my-key-pair"
+    security_groups = ["my-security-group"]
+ - tags:  = {
     Name = "example-instance"
   }
 }
