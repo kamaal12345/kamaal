39e89e228999d464b2c65217eb6335edcf633ab2
kamaal12345 <109724907+kamaal12345@users.noreply.github.com>
2023-04-04 13:19:18 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -6,13 +6,13 @@
 }
 
 # Create a new EC2 instance
--  resource:  "aws_instance" "example" {
+ resource  "aws_instance" "example" {
     ami           = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
     key_name      = "my-key-pair"
     vpc_security_group_ids = ["${aws_default_vpc.default.default_security_group_id}"]
     subnet_id     = "${aws_default_vpc.default.subnet_ids[0]}"
- - tags:  = {
+  tags  = {
     Name = "example-instance"
   }
 }
