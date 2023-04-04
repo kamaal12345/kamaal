29401cdfccd5ba8144fef0b44674219b7e4aa042
kamaal12345 <109724907+kamaal12345@users.noreply.github.com>
2023-04-04 12:53:21 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,18 +1,15 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+# Define the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
 
-  name = "single-instance"
-
-  ami                    = "ami-ami-00c39f71452c08778"
-  instance_type          = "t2.micro"
-  key_name               = "kamaal"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
+# Create a new EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-00c39f71452c08778"
+  instance_type = "t2.micro"
+  key_name      = "my-key-pair"
+  security_groups = ["my-security-group"]
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "example-instance"
   }
 }
