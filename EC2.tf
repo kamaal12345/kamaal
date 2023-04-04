57fcaac4747fd181fe29038648e0c326bc6c70f1
kamaal12345 <109724907+kamaal12345@users.noreply.github.com>
2023-04-04 12:26:51 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -6,6 +6,34 @@ provider "aws" {
 resource "aws_instance" "kamaal" {
   ami           = "ami-00c39f71452c08778"
   instance_type = "t2.micro"
-  encrypted     = true
-  
+   network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
 }
+
+ AutoScalingConfig:
+    Type: AWS::AutoScaling::LaunchConfiguration
+    Properties:
+      ...
+      BlockDeviceMappings:
+      - DeviceName: "/dev/sdk"
++       Ebs:
+          ...
++         Encrypted: true
+      - DeviceName: "/dev/sdf"
+        Ebs:
+            Encrypted: false
+            - DeviceName: "/dev/sdc"
+ VirtualName: ephermal
+ 
+
+
+
+
+
+
