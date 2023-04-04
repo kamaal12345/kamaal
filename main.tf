54f6a30acc91a5c2b71a6a5c20604822b48d8800
kamaal12345 <109724907+kamaal12345@users.noreply.github.com>
2023-04-04 14:13:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_instance" "project-iac" {
   ami = lookup(var.awsprops, "ami")
   instance_type = lookup(var.awsprops, "itype")
   subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
-  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  #associate_public_ip_address = lookup(var.awsprops, "publicip")
   key_name = lookup(var.awsprops, "keyname")
 
 
